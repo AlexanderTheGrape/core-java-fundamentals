ae2695e503f55f0d58492e1185fe4fde628ee9dd
Alexander Monk <amonk446@gmail.com>
2024-05-02 22:52:04 +1000

    Completed reflection class

--- reflection/Reflection.java
+++ reflection/Reflection.java
@@ -2,26 +2,31 @@ import java.util.Scanner;
 
 class Reflection {
 	
-	public static Array<?> createGenericArrayOfUserGivenType() throws Exception {
+	public static Object createGenericArrayOfUserGivenType() throws Exception {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the complete classname of the array type to create:");
 		String uncheckedStr = sc.next();
 		
 		// Create a class of given fully qualified name for a class, throw error if invalid
 		Class unknownClass = null;
+		Object myArray = null;
 		try {
 			unknownClass = Class.forName(uncheckedStr);
-		} catch (LinkageError|ExceptionInInitializerError|ClassNotFoundException ex) {
+			// Create array of given class type
+			myArray = java.lang.reflect.Array.newInstance(unknownClass, 10);
+		} catch (Exception ex) {
 			System.out.println(ex);
+			// TODO throw an exception
+			throw new Exception();
 		}
 		
-		// Create array of given class type
-		Array<?> myArray = java.util.reflect.Array.newInstance(unknownClass, 10);
+		return myArray;
 	}
 	
-	public static void main(String[] args) {
-		Array<?> arr = createGenericArrayOfUserGivenType();
-		System.out.println(java.util.Arrays.toList(arr));
+	public static void main(String[] args) throws Exception {
+		Object arr = createGenericArrayOfUserGivenType();
+		System.out.println(((List<java.util.Arrays.asList(arr)).toString());
+		System.out.println(arr.toString());
 	}
 	
 }
