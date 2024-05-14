e99b7d0c6652be4c0370947db7b2baefb956bac6
Alexander Monk <amonk446@gmail.com>
2024-05-14 19:26:08 +1000

    Added StringSorting class with Comparator example and usage

--- sorting/StringSorting.java
+++ sorting/StringSorting.java
@@ -1,18 +1,31 @@
+import java.util.Comparator;
+import java.util.Arrays;
 
-
-static class StringSorting {
-	
-	static class SortStringByLengthComparator implements Comparator{
-		static int compare(String s1, String s2) {
-			return s1.length() - s2.length();
-		}
+class StringSorting
+		implements Comparator<String> {
+			
+	public StringSorting(){}
+		
+	public int compare(String s1, String s2) {
+		return s1.length() - s2.length();
 	}	
 	
 	public static void main(String[] args) {
-		String[] strArr1 = new String[]{"a", "b", "c"};
-		String[] strArr2 = new String[]{"x", "y", "z"};
-		String[] strArr3 = new String[]{"a", "y", "x"};
-		String[] strArr3 = new String[]{"1", "2", "3"};
+		String[] strArr = {"ab", "bbb", "cb"};
+		String[] strArr2 = {"yy", "zzz", "x"};
+		String[] strArr3 = {"a", "y", "x"};
+		String[] strArr4 = {"1", "2", "3"};
+		
+		StringSorting comparator = new StringSorting();
+		Arrays.sort(strArr, comparator);
+		System.out.println(comparator.compare(strArr[0], strArr[1]));
+		System.out.println(Arrays.asList(strArr).toString());
+		Arrays.sort(strArr2, comparator);
+		System.out.println(Arrays.asList(strArr2).toString());
+		Arrays.sort(strArr3, comparator);
+		System.out.println(Arrays.asList(strArr3).toString());
+		Arrays.sort(strArr4, comparator);
+		System.out.println(Arrays.asList(strArr4).toString());
 	}
 	
 }
